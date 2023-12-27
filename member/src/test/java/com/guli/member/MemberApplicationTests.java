953103b26bb4177da985a4112c0ebb10c7a84e70
csy <Yixxxiu@gmail.com>
2023-12-27 17:10:51 +0800

    Create project structure

---
+++ member/src/test/java/com/guli/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guli.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
