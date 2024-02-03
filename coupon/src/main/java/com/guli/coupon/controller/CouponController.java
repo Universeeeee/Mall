86250132b3b8ecb784c33415d2a647b476e443a7
csy <304116359@qq.com>
2024-02-03 11:23:24 +0800

    新增nacos配置，并且进行集群分组配置

--- coupon/src/main/java/com/guli/coupon/controller/CouponController.java
+++ coupon/src/main/java/com/guli/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +28,20 @@ import com.guli.common.utils.R;
  */
 @RestController
 @RequestMapping("coupon/coupon")
+@RefreshScope
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private String age;
+
+    @RequestMapping("/user")
+    public R getUser(){
+        return R.ok().put("name",name).put("age",age);
+    }
 
     @RequestMapping("/member/couponList")
     public R memberCoupons(){
